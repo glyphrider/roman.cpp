cf1e1c618d98945aff0d35cb12ed5b8f37223159
Brian H. Ward <glyphrider@gmail.com>
2019-05-20 17:41:52 -0400

    static method with static local for initialization of const reference table

--- roman.cc
+++ roman.cc
@@ -14,26 +14,30 @@ bool Roman::Mapping::should_recurse(const std::string& s) const
 	return 0 == s.compare(0,second.size(),second);
 }
 
-const Roman::Table Roman::_table =
+const Roman::Table& Roman::_table()
 {
-  {1000,"M"},
-  {900,"CM"},
-  {500,"D"},
-  {400,"CD"},
-  {100,"C"},
-  {90,"XC"},
-  {50,"L"},
-  {40,"XL"},
-  {10,"X"},
-  {9,"IX"},
-  {5,"V"},
-  {4,"IV"},
-  {1,"I"}
-};
+	static Roman::Table _static = 
+	{
+    {1000,"M"},
+    {900,"CM"},
+    {500,"D"},
+    {400,"CD"},
+    {100,"C"},
+    {90,"XC"},
+    {50,"L"},
+    {40,"XL"},
+    {10,"X"},
+    {9,"IX"},
+    {5,"V"},
+    {4,"IV"},
+    {1,"I"}
+  };
+	return _static;
+}
 
 std::string Roman::to_roman(int n, const std::string& s) const
 {
-  for(const Mapping& t:_table)
+  for(const Mapping& t:_table())
 	{
 		if(t.should_recurse(n))
 		{
@@ -45,7 +49,7 @@ std::string Roman::to_roman(int n, const std::string& s) const
 
 int Roman::from_roman(const std::string& s, int n) const
 {
-  for(const Mapping& t:_table)
+  for(const Mapping& t:_table())
 	{
 		if(t.should_recurse(s))
 		{
--- roman.h
+++ roman.h
@@ -19,7 +19,7 @@ protected:
 		bool should_recurse(const std::string& s) const;
 	};
   using Table = std::list<Mapping>;
-  static const Table _table;
+  static const Table& _table();
   std::string to_roman(int n, const std::string& s) const;
   int from_roman(const std::string& s, int n) const;
 };
