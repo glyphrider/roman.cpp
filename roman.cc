53149c6f3ee5f99ccf713a511da59e25c8751365
Brian H. Ward <glyphrider@gmail.com>
2019-05-20 10:45:05 -0400

    refactoring of translation table into its own class and static instance

--- roman.cc
+++ roman.cc
@@ -1,20 +1,26 @@
 #include "roman.h"
 
+Roman::Translation Roman::_table;
+
+Roman::Translation::Translation()
+{
+  push_back(Mapping(1000,"M"));
+  push_back(Mapping(900,"CM"));
+  push_back(Mapping(500,"D"));
+  push_back(Mapping(400,"CD"));
+  push_back(Mapping(100,"C"));
+  push_back(Mapping(90,"XC"));
+  push_back(Mapping(50,"L"));
+  push_back(Mapping(40,"XL"));
+  push_back(Mapping(10,"X"));
+  push_back(Mapping(9,"IX"));
+  push_back(Mapping(5,"V"));
+  push_back(Mapping(4,"IV"));
+  push_back(Mapping(1,"I"));
+}
+
 Roman::Roman()
 {
-  _table.push_back(Mapping(1000,"M"));
-  _table.push_back(Mapping(900,"CM"));
-  _table.push_back(Mapping(500,"D"));
-  _table.push_back(Mapping(400,"CD"));
-  _table.push_back(Mapping(100,"C"));
-  _table.push_back(Mapping(90,"XC"));
-  _table.push_back(Mapping(50,"L"));
-  _table.push_back(Mapping(40,"XL"));
-  _table.push_back(Mapping(10,"X"));
-  _table.push_back(Mapping(9,"IX"));
-  _table.push_back(Mapping(5,"V"));
-  _table.push_back(Mapping(4,"IV"));
-  _table.push_back(Mapping(1,"I"));
 }
 
 Roman::~Roman()
@@ -23,7 +29,7 @@ Roman::~Roman()
 
 std::string Roman::to_roman(int n, std::string s)
 {
-  for(auto t:_table)
+  for(auto t:Roman::_table)
     if(n >= t.first)
       return to_roman(n-t.first,s+t.second);
   return s;
@@ -31,7 +37,7 @@ std::string Roman::to_roman(int n, std::string s)
 
 int Roman::from_roman(std::string s, int n)
 {
-  for(auto t:_table)
+  for(auto t:Roman::_table)
     if(0 == s.compare(0,t.second.size(),t.second))
       return from_roman(s.substr(t.second.size()),n+t.first);
   return n;
