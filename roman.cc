3c4fe7deb2a2323ecb9b1b5fd4a7043a711c63d6
Brian H. Ward <glyphrider@gmail.com>
2019-05-23 10:23:32 -0400

    reformat buffers

--- roman.cc
+++ roman.cc
@@ -6,56 +6,56 @@ Roman::Mapping::Mapping(int n, const std::string& s) : Roman::StdMapping(n,s)
 
 bool Roman::Mapping::should_recurse(int n) const
 {
-	return n >= first;
+  return n >= first;
 }
 
 bool Roman::Mapping::should_recurse(const std::string& s) const
 {
-	return 0 == s.compare(0,second.size(),second);
+  return 0 == s.compare(0,second.size(),second);
 }
 
 const Roman::Table& Roman::table() const
 {
-	static Roman::Table _static = 
-	{
-    {1000,"M"},
-    {900,"CM"},
-    {500,"D"},
-    {400,"CD"},
-    {100,"C"},
-    {90,"XC"},
-    {50,"L"},
-    {40,"XL"},
-    {10,"X"},
-    {9,"IX"},
-    {5,"V"},
-    {4,"IV"},
-    {1,"I"}
-  };
-	return _static;
+  static Roman::Table _static = 
+    {
+      {1000,"M"},
+      {900,"CM"},
+      {500,"D"},
+      {400,"CD"},
+      {100,"C"},
+      {90,"XC"},
+      {50,"L"},
+      {40,"XL"},
+      {10,"X"},
+      {9,"IX"},
+      {5,"V"},
+      {4,"IV"},
+      {1,"I"}
+    };
+  return _static;
 }
 
 std::string Roman::to_roman(int n, const std::string& s) const
 {
   for(const Mapping& t:table())
+    {
+      if(t.should_recurse(n))
 	{
-		if(t.should_recurse(n))
-		{
-      return to_roman(n-t.first,s+t.second);
-		}
+	  return to_roman(n-t.first,s+t.second);
 	}
+    }
   return s;
 }
 
 int Roman::from_roman(const std::string& s, int n) const
 {
   for(const Mapping& t:table())
+    {
+      if(t.should_recurse(s))
 	{
-		if(t.should_recurse(s))
-		{
-      return from_roman(s.substr(t.second.size()),n+t.first);
-		}
+	  return from_roman(s.substr(t.second.size()),n+t.first);
 	}
+    }
   return n;
 }
 
--- roman.h
+++ roman.h
@@ -5,19 +5,19 @@
 #include <string>
 
 class Roman {
-public:
+ public:
   Roman() = default;
   virtual ~Roman() = default;
   std::string to_s(int n) const;
   int to_i(const std::string& s) const;
-protected:
+ protected:
   using StdMapping = std::pair<int,std::string>;
-	class Mapping : public StdMapping {
-		public:
-		Mapping(int n, const std::string& s);
-		bool should_recurse(int n) const;
-		bool should_recurse(const std::string& s) const;
-	};
+  class Mapping : public StdMapping {
+  public:
+    Mapping(int n, const std::string& s);
+    bool should_recurse(int n) const;
+    bool should_recurse(const std::string& s) const;
+  };
   using Table = std::list<Mapping>;
   const Table& table() const;
   std::string to_roman(int n, const std::string& s) const;
