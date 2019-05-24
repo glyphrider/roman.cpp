1f865bd1cb0e60895e12f3b3225d7ea9a785b960
Brian H. Ward <glyphrider@gmail.com>
2019-05-24 14:27:27 -0400

    whitespace cleanup

--- roman.h
+++ roman.h
@@ -11,13 +11,13 @@ public:
   std::string to_s(int n) const;
   int to_i(const std::string& s) const;
 protected:
-  using StdMapping = std::pair<int,std::string>;
-	class Mapping : public StdMapping {
-		public:
-		Mapping(int n, const std::string& s);
-		bool should_recurse(int n) const;
-		bool should_recurse(const std::string& s) const;
-	};
+  using StdPair = std::pair<int,std::string>;
+  class Mapping : public StdPair {
+  public:
+    Mapping(int n, const std::string& s);
+    bool should_recurse(int n) const;
+    bool should_recurse(const std::string& s) const;
+  };
   using Table = std::list<Mapping>;
   const Table& table() const;
   std::string to_roman(int n, const std::string& s) const;
